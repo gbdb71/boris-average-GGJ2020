bd2a9bb96fd88b4afe4849aec5f24d455ad7e5d7
MSI <a10decastro@gmail.com>
2020-02-01 19:06:26 +0200

    almost playable version

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -6,12 +6,71 @@ public class Hand : MonoBehaviour
 {
     public enum toolType {Empty, Tape, Gum, Sock}
     public toolType handType = toolType.Empty;
+    public float repairCounter = 0.0f;
 
-    private void OnTriggerEnter(Collider other)
+    private bool leakBlocked = false;
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.layer == 9) {
-            Destroy(other.gameObject);
+
+            repairCounter += Time.deltaTime;
+
+            if (!leakBlocked)
+            {
+                // ParticleSystem leakParticleSystem = other.gameObject.transform.GetChild(0).GetComponent<ParticleSystem>();
+                // ParticleSystem blockedParticleSystem = other.gameObject.transform.GetChild(1).GetComponent<ParticleSystem>();
+                Transform leakParticle = other.gameObject.transform.GetChild(0);
+                Transform blockedParticle = other.gameObject.transform.GetChild(1);
+
+                leakParticle.gameObject.SetActive(false);
+                blockedParticle.gameObject.SetActive(true);
+
+                leakBlocked = true;
+            }
+
+            if (repairCounter > getToolTimer(handType))
+                destroyLeaking(other.gameObject);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            repairCounter = 0.0f;
+            leakBlocked = false;
+        }
+    }
+
+    private void destroyLeaking(GameObject leakingGO) {
+        repairCounter = 0.0f;
+        GameManager.Instance.levelManager.numOfLeakings--;
+        Destroy(leakingGO);
+    }
+
+
+    private float getToolTimer(toolType type) {
+        float timer = 0.0f;
+
+        switch (type)
+        {
+            case toolType.Empty:
+                timer = 0;
+                break; 
+            case toolType.Tape:
+                timer = 2;
+                break;
+            case toolType.Gum:
+                timer = 1;
+                break;
+            case toolType.Sock:
+                timer = 1.5f;
+                break;
+
+        }
+
+        return timer;
     }
 
 }
