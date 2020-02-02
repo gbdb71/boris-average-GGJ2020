12a166210ddc066ae27d24024304607b1c5d9a2b
Ivan Dmitriev <hiskind@gmail.com>
2020-02-02 11:37:31 +0200

    Death screen score support

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,26 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
+    [Header("Animator")]
     public Animator animator;
 
-    public void GameOver() {
+    [Header("Text")]
+    public TextMeshProUGUI textScore = null;
+
+    public void GameOver()
+    {
+        GameOver(0);
+    }
+        
+    public void GameOver(float score)
+    {
         animator.SetTrigger("Fade_In");
+        UpdateText(score);
     }
 
+
+    private void UpdateText(float score)
+    {
+        float seconds = score % 60;
+        float minutes = ((int)(score / 60) % 60);
+        textScore.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    } 
+
+    private void ClearText()
+    {
+        textScore.text = "";
+    } 
+
     public void GameRestart() {
-        Debug.Log("CLICK");
         animator.SetTrigger("Fade_Out");
     }
 
     public void Update() {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            GameOver();
+            GameOver(232);
 		}
     }
-
 }
