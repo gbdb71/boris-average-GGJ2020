7470fdcdb799d05adf19be53a8c5471786811ea4
Ivan Dmitriev <hiskind@gmail.com>
2020-02-02 10:38:23 +0200

    GameOver screen

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public Animator animator;
+
+    public void GameOver() {
+        animator.SetTrigger("Fade_In");
+    }
+
+    public void GameRestart() {
+        Debug.Log("CLICK");
+        animator.SetTrigger("Fade_Out");
+    }
+
+    public void Update() {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameOver();
+		}
+    }
+
+}
